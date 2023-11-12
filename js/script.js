78ad413810c834d97f4920af590cf691e1fb0ba4
nekkox <108139374+nekkox@users.noreply.github.com>
2023-11-12 16:54:18 +0100

    Create show  detalis function that fetches details from api

--- js/script.js
+++ js/script.js
@@ -4,21 +4,42 @@ const global = {
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
-    // Register your key at https://www.themoviedb.org/settings/api and enter here
-    // Only use this for development or very small projects. You should store your key and make requests from a server
-    const API_KEY = "";
-    const API_URL = "https://api.themoviedb.org/3/";
-  
-    showSpinner();
-    const response = await fetch(
-      `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
-    );
-  
-    const data = await response.json();
-  
-    hideSpinner();
-    return data;
+  // Register your key at https://www.themoviedb.org/settings/api and enter here
+  // Only use this for development or very small projects. You should store your key and make requests from a server
+  const API_KEY = "";
+  const API_URL = "https://api.themoviedb.org/3/";
+
+  showSpinner();
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+
+  hideSpinner();
+  return data;
+}
+
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement("div");
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = "cover";
+  overlayDiv.style.backgroundPosition = "center";
+  overlayDiv.style.backgroundRepeat = "no-repeat";
+  overlayDiv.style.height = "100vh";
+  overlayDiv.style.width = "100vw";
+  overlayDiv.style.position = "absolute";
+  overlayDiv.style.top = "0";
+  overlayDiv.style.left = "0";
+  overlayDiv.style.zIndex = "-1";
+  overlayDiv.style.opacity = "0.1";
+
+  if (type === "movie") {
+    document.querySelector("#movie-details").appendChild(overlayDiv);
+  } else {
+    document.querySelector("#show-details").appendChild(overlayDiv);
   }
+}
 
 function highlightActiveLink() {
   const links = document.querySelectorAll(".nav-link");
@@ -34,6 +55,9 @@ function highlightActiveLink() {
 async function displayMovieDetails() {
   const movieId = window.location.search.split("=")[1];
   const movie = await fetchAPIData(`movie/${movieId}`);
+
+  //Overlay for background image
+  displayBackgroundImage("movie", movie.backdrop_path);
 
   const div = document.createElement("div");
   div.innerHTML = `<div class="details-top">
@@ -95,7 +119,75 @@ async function displayMovieDetails() {
   console.log(movie);
 }
 
-
+// Display Show Details
+async function displayShowDetails() {
+    const showId = window.location.search.split('=')[1];
+  
+    const show = await fetchAPIData(`tv/${showId}`);
+  console.log(show);
+    // Overlay for background image
+    displayBackgroundImage('tv', show.backdrop_path);
+  
+    const div = document.createElement('div');
+  
+    div.innerHTML = `
+    <div class="details-top">
+    <div>
+    ${
+      show.poster_path
+        ? `<img
+      src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+      class="card-img-top"
+      alt="${show.name}"
+    />`
+        : `<img
+    src="../images/no-image.jpg"
+    class="card-img-top"
+    alt="${show.name}"
+  />`
+    }
+    </div>
+    <div>
+      <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+      <p>
+        ${show.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="${
+        show.homepage
+      }" target="_blank" class="btn">Visit show Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+      <li><span class="text-secondary">Number of Episodes:</span> ${
+        show.number_of_episodes
+      }</li>
+      <li><span class="text-secondary">Last Episode To Air:</span> ${
+        show.last_episode_to_air.name
+      }</li>
+      <li><span class="text-secondary">Status:</span> ${show.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+      ${show.production_companies
+        .map((company) => `<span>${company.name}</span>`)
+        .join(', ')}
+    </div>
+  </div>
+    `;
+  
+    document.querySelector('#show-details').appendChild(div);
+  }
 
 function showSpinner() {
   document.querySelector(".spinner").classList.add("show");
@@ -196,8 +288,10 @@ function Init() {
     case "/shows.html":
       displayPopularShows();
       break;
-    case global.currentPage.includes("/tv-details.html"):
+    case "/tv-details.html":
       console.log("Show Details");
+      displayShowDetails();
+      console.log(global);
       break;
     case "/movie-details.html":
       console.log("Movie Details");
